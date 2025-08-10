2f00acc9d2cbb74297d6fb1fdc2c4035488cad12
Mario Cobian <marvanis@hotmail.com>
2025-08-10 00:48:19 -0600

    Rutas con nombres implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -4,6 +4,9 @@ import 'package:widgets_app/config/menu/menu_items.dart';
 
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -60,6 +63,7 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.pushNamed(context, menuItem.link);
 
         context.push(menuItem.link);
+        
       },
     );
   }
